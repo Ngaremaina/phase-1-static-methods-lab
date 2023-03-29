324823933ed89e7793fa6f0393c7ed4323f2494a
Ngaremaina <ngaremaina4@gmail.com>
2023-03-30 00:12:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize (str){
+    return str[0].toUpperCase() + str.substring(1)
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+  static titleize(str){
+    let sentence = str[0].toUpperCase()
+    let word = str.split(" ").map( value => {
+
+      if (value === "the" || value === "a" || value === 'an' || value === 'but'
+      || value === 'of'|| value ==='and'|| value ==='for'|| value === 'at'|| value ==='by'
+      || value === 'from'){
+        return value.charAt(0).toLowerCase() + value.substring(1)
+      }
+      
+      return value.charAt(0).toUpperCase() + value.substring(1)
+
+  }).join(" ")
+
+  return sentence + word.substring(1)
+  }
+  
 }
